fb7b5288263ea6bb5e40bf5ebb5380beb95d0e25
Aleksandar Dimitrov <alexander.d.dimitrov@gmail.com>
2026-01-25 12:35:03 +0200

    Refactoring - polished error messages for "Add Cat" form

--- PurrfectShot.Web/ViewModels/Cats/CatFormViewModel.cs
+++ PurrfectShot.Web/ViewModels/Cats/CatFormViewModel.cs
@@ -5,15 +5,16 @@ namespace PurrfectShot.Web.ViewModels.Cats
 {
     public class CatFormViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Всяко коте си има име!")]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = "Името трябва да е между {2} и {1} символа.")]
         public string Name { get; set; } = null!;
 
-        [Required]
-        [StringLength(BreedMaxLength, MinimumLength = BreedMinLength)]
+        [Required(ErrorMessage = "Породата е задължителна!")]
+        [StringLength(BreedMaxLength, MinimumLength = BreedMinLength, ErrorMessage = "Породата трябва да е между {2} и {1} символа.")]
         public string Breed { get; set; } = null!;
 
-        [Required]
-        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        [Required(ErrorMessage = "Описанието е задължително!")]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = "Описанието трябва да е между {2} и {1} символа.")]
         public string Description { get; set; } = null!;
     }
 }
